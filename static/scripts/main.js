ce5b89a3d0f0548ca50e120314d1092b40fb4efd
Camilo Mendoza <camilomen@localhost.localdomain>
2024-09-11 17:40:36 -0400

    Proyecto base

---
+++ static/scripts/main.js
@@ -0,0 +1,21 @@
+const dots = document.getElementsByClassName("dot");
+const slides = document.getElementsByClassName("hero-slide");
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
